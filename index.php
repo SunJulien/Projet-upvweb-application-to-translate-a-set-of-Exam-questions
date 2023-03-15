4d5c781af6d5fb44607e20c7c917c75f430e9df0
SunJulien <julien.sun@groupe-esigelec.org>
2023-03-15 10:26:08 +0100

    first

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>upvsun website</title>
+	</head>
+	<body>
+		<h1><?php echo "Hello World!"; ?></h1>
+
+		<p>This is the landing page of <strong>upvsun</strong>.</p>
+        <button onclick="window.location.href='info.php'">info of serveur</button>
+        <button onclick="window.location.href='xdebug.php'">info of serveur</button>
+        <button onclick="window.location.href='todo_list.php'">info of serveur</button>
+
+    </body>
+</html>
